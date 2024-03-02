2a4af1a378a9a4ca8e9fa0237cd9bb74ba79cce6
khang <enone859@gmail.com>
2024-03-02 18:10:52 -0500

    turn off secret scanning

--- main.tf
+++ main.tf
@@ -28,6 +28,12 @@ resource "github_repository" "self" {
   vulnerability_alerts   = var.vulnerability_alerts
   archived               = var.archived
 
+  security_and_analysis {
+    secret_scanning {
+      status = false
+    }
+  }
+
   dynamic "pages" {
     for_each = var.enable_pages ? [1] : []
 
